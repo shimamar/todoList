2175c101d221184f9232680d72db3c255b76f68b
shimamar <mari.shimada4@gmail.com>
2020-05-04 15:18:43 +0900

    ブラウザ表示修正

--- app/views/todos/index.php
+++ app/views/todos/index.php
@@ -6,49 +6,10 @@ require 'config/db.php';
 //DBデータ取得・更新ファイル
 require 'models/todo.php';
 
-//MySQLと接続
-try {
-    // PDOインスタンスを生成
-    $dbh = new PDO($dsn, $user, $password);
-} catch (PDOException $e) {
-    // エラーメッセージ表示
-    echo 'データベースにアクセスできません！' . $e->getMessage();
-    // 強制終了
-    exit;
-}
-
-$todo = new Todo();
+//ユーザーID
+$user_id = 1;
 /* todos */
-$sql = "SELECT * FROM sample.todos";
-$todo_list = $todo->findAll($dbh, $sql);
-
-/* user */
-$sql = "SELECT * FROM sample.users";
-$user_list = $todo->findAll($dbh, $sql);
-
-/* todos */
-//SQL実行
-/*
-try {
-    //SQL文の組み立て
-    $sql = "SELECT * FROM sample.todos";
-    $stmh = $dbh->query($sql);
-} catch (PDOException $e) {
-    echo '接続エラー：' . $e->getMessage();
-}
-
-//値を取得
-$todo_list = $stmh->fetchAll(PDO::FETCH_ASSOC);*/
-
-/* user */
-/*try {
-    $sql = "SELECT * FROM sample.users";
-    $stmh = $dbh->query($sql);
-} catch (PDOException $e) {
-    echo '接続エラー：' . $e->getMessage();
-}
-
-$user_list = $stmh->fetchAll(PDO::FETCH_ASSOC);*/
+$todo_list = Todo::findAll($dsn, $user, $password, $user_id);
 
  ?>
 
@@ -77,26 +38,16 @@ $user_list = $stmh->fetchAll(PDO::FETCH_ASSOC);*/
         <th><?=htmlspecialchars($todo['deadline_date'])?></th>
         <th><?=htmlspecialchars($todo['statue'])?></th>
         <th><?=htmlspecialchars($todo['created_at'])?></th>
-        <th><?=htmlspecialchars($todo['updated_at'])?></th>
-        <th><?=htmlspecialchars($todo['deleted_at'])?></th>
-    </tr>
-<?php endforeach;?>
-</tbody></table>
-
-<h2>USERリスト</h2>
-<table><tbody>
-    <tr>
-        <th>ID</th>
-        <th>name</th>
-        <th>created_at</th>
-        <th>updated_at</th>
-    </tr>
-<?php foreach($user_list as $user):?>
-    <tr>
-        <th><?=htmlspecialchars($user['id'])?></th>
-        <th><?=htmlspecialchars($user['name'])?></th>
-        <th><?=htmlspecialchars($user['created_at'])?></th>
-        <th><?=htmlspecialchars($user['updated_at'])?></th>
+        <?php if($todo['updated_at']) : ?>
+            <th><?=htmlspecialchars($todo['updated_at'])?></th>
+        <?php else : ?>
+            <th>データなし</th>
+        <?php endif; ?>
+        <?php if($todo['deleted_at']) : ?>
+            <th><?=htmlspecialchars($todo['deleted_at'])?></th>
+        <?php else : ?>
+            <th>データなし</th>
+        <?php endif; ?>
     </tr>
 <?php endforeach;?>
 </tbody></table>
--- app/views/todos/models/todo.php
+++ app/views/todos/models/todo.php
@@ -2,7 +2,19 @@
 
 class Todo {
 
-    public function findAll($dbh, $sql){
+    public static function findAll($dsn, $user, $password, $user_id){
+
+        //MySQLと接続
+        try {
+            // PDOインスタンスを生成
+            $dbh = new PDO($dsn, $user, $password);
+        } catch (PDOException $e) {
+            // エラーメッセージ表示
+            echo 'データベースにアクセスできません！' . $e->getMessage();
+            // 強制終了
+            exit;
+        }
+        $sql = "SELECT * FROM sample.todos WHERE user_id=" . $user_id;
 
         //SQL実行
         try {
@@ -12,14 +24,32 @@ class Todo {
             echo '接続エラー：' . $e->getMessage();
         }
 
+        //$stmh 空チェック
+        if($stmh){
+            exit;
+        }
         //値を取得
-        $data_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
-
-        return $this->data_list = $data_list;
-
+        return $data_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
     }
 
 
+    public static function findByQuery($dsn, $user, $password, $sql) {
+        //MySQLと接続
+        try {
+            $dbh = new PDO($dsn, $user, $password);
+        } catch (PDOException $e) {
+            echo 'データベースにアクセスできません！' . $e->getMessage();
+            exit;
+        }
+        //SQL実行
+        try {
+            $stmh = $dbh->query($sql);
+        } catch (PDOException $e) {
+            echo '接続エラー：' . $e->getMessage();
+        }
+        //値を取得
+        return $data_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
+    }
 
 }
 
